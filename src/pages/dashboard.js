7ebdba19308968b8895754cd23013e8a677e029a
Jose-Mu <56739324+Jose-Mu@users.noreply.github.com>
2021-11-23 20:17:41 +0700

    main

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,6 +4,9 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function Dashboard () {
   return (
     <View style={styles.container}>
+      <View>
+      <Text style={styles.name}>Ohayou, James-kun</Text>
+      </View>
       <Text>Dashboard - Silahkan Mengubah Halaman Sesuai Figma</Text>
     </View>
   );
@@ -15,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  name:{
+    fontWeight: 'bold',
+    fontSize: 30,
+    alignSelf: 'flex-start'
   },
 });
