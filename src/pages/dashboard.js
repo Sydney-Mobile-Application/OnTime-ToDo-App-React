ac2cdeea32b97f2f2b85c5b02b1eb5edf4a761e4
Steven Salim <steven.salim@grip-principle.com>
2021-12-01 18:13:31 +0700

    Fix Error Caused by wrong value on styling

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,250 +1,255 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image, Pressable, Dimensions, Switch} from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Pressable,
+  Dimensions,
+  Switch,
+} from "react-native";
 
-const width_name = '60%';
-const width_highlight = '75%';
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const width_name = "60%";
+const width_highlight = "75%";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
-export default function Dashboard () {
+export default function Dashboard() {
   return (
     <View>
-
       <View style={styles.top}>
-          <View style={styles.left}>
-            <Text style={styles.name}>Ohayou, James-kun!</Text>
-            <Text style={styles.date}>20 Jun</Text>
-          </View>
+        <View style={styles.left}>
+          <Text style={styles.name}>Ohayou, James-kun!</Text>
+          <Text style={styles.date}>20 Jun</Text>
+        </View>
 
-          <View style={styles.right}>
-            <Image style={styles.avatar} source={require('../../assets/profile1.jpeg')} />
-            <Text style={styles.notif}>20 tasks to do today</Text>
-          </View>
+        <View style={styles.right}>
+          <Image
+            style={styles.avatar}
+            source={require("../../assets/profile1.jpeg")}
+          />
+          <Text style={styles.notif}>20 tasks to do today</Text>
+        </View>
       </View>
-      
 
       <View style={styles.container}>
-
         <View style={styles.highlight}>
           <Text style={styles.highlight_text}>Meeting with project team</Text>
         </View>
 
-        
-        <Text style={styles.priority} >Priority</Text>
+        <Text style={styles.priority}>Priority</Text>
 
-          <View style={styles.priorityCont}>
-            <View style={styles.taskNear}>
-              <Text style={styles.taskText}>Meeting with Project Team</Text>
-              <Text style={styles.taskDate}>20 Sep</Text>
-            </View>
-            <View style={styles.taskNear2}>
-              <Text style={styles.taskText}>Kerjakan Tugas PAM</Text>
-              <Text style={styles.taskDate}>26 Sep</Text>
-            </View>
+        <View style={styles.priorityCont}>
+          <View style={styles.taskNear}>
+            <Text style={styles.taskText}>Meeting with Project Team</Text>
+            <Text style={styles.taskDate}>20 Sep</Text>
           </View>
-
-        <Text style={styles.upcoming} >Upcoming</Text>
-
-          <View style={styles.upcomingCont}>
-            <View style={styles.taskCommon}>
-              <Text style={styles.taskText}>Meeting with Project Team</Text>
-              <Text style={styles.taskDate}>20 Sep</Text>
-            </View>
-            <View style={styles.taskCommon2}>
-              <Text style={styles.taskText}>Kerjakan Tugas PAM</Text>
-              <Text style={styles.taskDate}>26 Sep</Text>
-            </View>
+          <View style={styles.taskNear2}>
+            <Text style={styles.taskText}>Kerjakan Tugas PAM</Text>
+            <Text style={styles.taskDate}>26 Sep</Text>
           </View>
+        </View>
 
+        <Text style={styles.upcoming}>Upcoming</Text>
+
+        <View style={styles.upcomingCont}>
+          <View style={styles.taskCommon}>
+            <Text style={styles.taskText}>Meeting with Project Team</Text>
+            <Text style={styles.taskDate}>20 Sep</Text>
+          </View>
+          <View style={styles.taskCommon2}>
+            <Text style={styles.taskText}>Kerjakan Tugas PAM</Text>
+            <Text style={styles.taskDate}>26 Sep</Text>
+          </View>
+        </View>
       </View>
-
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:30,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  priorityHead:{
-    fontWeight: 'bold',
+  priorityHead: {
+    fontWeight: "bold",
     fontSize: 18,
-    marginBottom:30,
+    marginBottom: 30,
     marginLeft: 20,
-    alignSelf: 'flex-start',
-    alignItems: 'left',
-    justifyContent: 'left',
+    alignSelf: "flex-start",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
-  priority:{
-    fontWeight: 'bold',
+  priority: {
+    fontWeight: "bold",
     fontSize: 18,
-    marginBottom:30,
+    marginBottom: 30,
     marginLeft: 20,
-    alignSelf: 'flex-start',
-    alignItems: 'left',
-    justifyContent: 'left',
+    alignSelf: "flex-start",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
-  upcoming:{
-    fontWeight: 'bold',
+  upcoming: {
+    fontWeight: "bold",
     fontSize: 18,
-    marginBottom:30,
+    marginBottom: 30,
     marginLeft: 20,
-    alignSelf: 'flex-start',
-    alignItems: 'left',
-    justifyContent: 'left',
+    alignSelf: "flex-start",
+    // alignItems: 'left',
+    // justifyContent: "flex-start",
   },
-  priorityCont:{
-    marginBottom:30,
-    flexDirection: 'row',
+  priorityCont: {
+    marginBottom: 30,
+    flexDirection: "row",
   },
-  upcomingCont:{
-    marginBottom:30,
-    flexDirection: 'row',
+  upcomingCont: {
+    marginBottom: 30,
+    flexDirection: "row",
   },
-  taskNear:{
-    width:131,
-    height:131,
+  taskNear: {
+    width: 131,
+    height: 131,
     marginRight: 20,
     borderRadius: 20,
-    paddingTop:30,
-    paddingBottom:30,
-    paddingLeft:15,
-    paddingRight:15,
-    alignSelf: 'flex-start',
-    alignItems: 'center',
-    justifyContent: 'left',
-    backgroundColor: '#293462',
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignSelf: "flex-start",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#293462",
   },
-  taskNear2:{
-    width:131,
-    height:131,
+  taskNear2: {
+    width: 131,
+    height: 131,
     marginLeft: 20,
     borderRadius: 20,
-    paddingTop:30,
-    paddingBottom:30,
-    paddingLeft:15,
-    paddingRight:15,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-    justifyContent: 'left',
-    backgroundColor: '#EE6F57',
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#EE6F57",
   },
-  taskCommon:{
-    width:131,
-    height:131,
+  taskCommon: {
+    width: 131,
+    height: 131,
     marginRight: 20,
     borderRadius: 20,
-    paddingTop:30,
-    paddingBottom:30,
-    paddingLeft:15,
-    paddingRight:15,
-    alignSelf: 'flex-start',
-    alignItems: 'center',
-    justifyContent: 'left',
-    backgroundColor: '#5089C6',
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignSelf: "flex-start",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#5089C6",
   },
-  taskCommon2:{
-    width:131,
-    height:131,
+  taskCommon2: {
+    width: 131,
+    height: 131,
     marginLeft: 20,
     borderRadius: 20,
-    paddingTop:30,
-    paddingBottom:30,
-    paddingLeft:15,
-    paddingRight:15,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-    justifyContent: 'left',
-    backgroundColor: '#5089C6',
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#5089C6",
   },
-  taskText:{
-    color: '#FFFFFF',
+  taskText: {
+    color: "#FFFFFF",
   },
-  taskDate:{
+  taskDate: {
     marginTop: 10,
-    color: '#FFFFFF',
-    alignSelf: 'flex-start',
-    fontWeight: 'bold',
+    color: "#FFFFFF",
+    alignSelf: "flex-start",
+    fontWeight: "bold",
     fontSize: 22,
   },
   avatar: {
     width: 55,
     height: 55,
     borderRadius: 50,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     marginRight: 8,
     marginBottom: 5,
   },
-  notif:{
+  notif: {
     fontSize: 13,
   },
   top: {
-    backgroundColor: '#FBFBFB',
-    alignItems: 'center',
-    justifyContent: 'left',
-    flexDirection:'row',
-    paddingBottom:20,
+    backgroundColor: "#FBFBFB",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    paddingBottom: 20,
   },
   left: {
-    backgroundColor: '#FBFBFB',
+    backgroundColor: "#FBFBFB",
     alignSelf: "flex-start",
-    alignItems: 'left',
-    justifyContent: 'left',
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
   right: {
-    backgroundColor: '#FBFBFB',
+    backgroundColor: "#FBFBFB",
     alignSelf: "flex-end",
-    alignItems: 'left',
-    justifyContent: 'left',
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
-  name:{
+  name: {
     marginTop: 20,
     marginLeft: 25,
     width: width_name,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 19,
-    alignSelf: 'flex-start'
+    alignSelf: "flex-start",
   },
-  taskp:{
-    alignItems: 'right',
-    justifyContent: 'right',
+  taskp: {
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
     fontSize: 12,
-    alignSelf: 'flex-start'
+    alignSelf: "flex-start",
   },
-  date:{
+  date: {
     marginTop: 17,
     marginLeft: 19,
     borderRadius: 15,
-    paddingLeft:10,
-    paddingRight:10,
-    backgroundColor: '#fff',
-    fontWeight: 'bold',
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#fff",
+    fontWeight: "bold",
     fontSize: 18,
-    alignSelf: 'flex-start',
-    shadowColor: '#000',
+    alignSelf: "flex-start",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.5,
-    shadowRadius: 2,  
-    elevation: 5
+    shadowRadius: 2,
+    elevation: 5,
   },
-  highlight:{
+  highlight: {
     margin: 20,
     borderRadius: 20,
-    paddingTop:30,
-    paddingBottom:30,
-    paddingLeft:15,
-    paddingRight:15,
-    backgroundColor: '#FFEDBF',
-    
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+    backgroundColor: "#FFEDBF",
   },
-  highlight_text:{
-    width:width_highlight,
-    alignItems: 'center',
-    justifyContent: 'left',
-    fontSize: 19,    
-    fontWeight: 'bold', 
+  highlight_text: {
+    width: width_highlight,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    fontSize: 19,
+    fontWeight: "bold",
   },
 });
--- src/pages/toDoPriority.js
+++ src/pages/toDoPriority.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function ToDoPriority () {
+export default function ToDoPriority() {
   return (
     <View style={styles.container}>
       <Text>To Do Priority - Silahkan Mengubah Halaman Sesuai Figma</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
