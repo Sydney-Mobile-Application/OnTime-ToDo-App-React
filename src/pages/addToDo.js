bb553fee8232cd39ca3b0dc550a2087c105efef6
Jose-Mu <56739324+Jose-Mu@users.noreply.github.com>
2021-12-08 19:53:24 +0700

    main

--- src/pages/addToDo.js
+++ src/pages/addToDo.js
@@ -9,7 +9,8 @@ const windowHeight = Dimensions.get("window").height;
 export default function AddToDo () {
   const [textEmail, onChangeTextEmail] = useState("");
   return (
-    <View>
+    <View style={styles.container}>
+
       <View style={styles.containertop}>
         <View style={styles.back} >
           <MaterialIcons name='arrow-back' size={30} color='#293462'/>
@@ -21,25 +22,33 @@ export default function AddToDo () {
       <View style={styles.task}>
           <TextInput style={styles.title} onChangeText={onChangeTextEmail}  placeholder="Title " />
           <TextInput style={styles.description} onChangeText={onChangeTextEmail} multiline={true} placeholder="Description " />
-        </View>
+      </View>  
       <View style={styles.containerBottom}>
-          <MaterialIcons  name='font-download' size={30} color='#293462'/>
-          <Text>Add To Do - Silahkan Mengubah Halaman Sesuai Figma</Text>
-
+        <MaterialIcons  name='font-download' size={30} color='#293462' style={styles.font}/>
+        <MaterialIcons  name='calendar-today' size={30} color='#293462' style={styles.font}/>
+        <Text style={styles.saveButton}>Save</Text>
+        
       </View>
-    
-    </View>
+
+    </View> 
+      
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
   containerBottom: {
     // flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
-    flexDirection:'column',
-    height: windowHeight,
+    flexDirection:'row',
+    // height: windowHeight,
   },
   containertop: {
     width: windowWidth,
@@ -66,7 +75,7 @@ const styles = StyleSheet.create({
     marginTop: windowHeight*0.05,
     marginLeft:  windowWidth*0.1,
     marginRight:  windowWidth*0.08,
-    height: windowHeight*0.75,
+    height: windowHeight*0.68,
   },
   title: {
     fontSize: 30,
@@ -83,5 +92,29 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "flex-start",
     // maxHeight: windowHeight*0.8,
+  },
+  font: {
+    alignSelf: "flex-start",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginLeft: windowWidth*0.1,
+  },
+  saveButton: {
+    marginLeft: windowWidth*0.35 ,
+    borderRadius: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 5,
+    paddingBottom: 5,
+    color: "#fff",
+    backgroundColor: "#293462",
+    fontWeight: "bold",
+    fontSize: 15,
+    alignSelf:'flex-end',
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 5,
   },
 });
